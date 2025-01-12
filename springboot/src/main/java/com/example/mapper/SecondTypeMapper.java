1ab0500e896efdee90416c63acf75a007af74366
sgl <sgl17600726166@163.com>
2025-01-13 00:21:41 +0800

    二级分类实现了一半

---
+++ springboot/src/main/java/com/example/mapper/SecondTypeMapper.java
@@ -0,0 +1,14 @@
+package com.example.mapper;
+
+import com.example.entity.SecondType;
+
+import java.util.List;
+
+public interface SecondTypeMapper {
+    List<SecondType> selectAll(SecondType secondType);
+    List<SecondType> selectByTypeId(Integer typeId);
+    SecondType selectById(Integer id);
+    void insert(SecondType secondType);
+    void updateById(SecondType secondType);
+    void deleteById(Integer id);
+}
